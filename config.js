ad145aabc5c64b1d14b7d6317c6c0ff1cc564b89
hanguyen-phu <phuha2002k57@gmail.com>
2023-12-07 17:40:51 +0700

    goodluck

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "For Thũy Ngân";
+// const giftUrl = 'http://nodemy.vn';
+const eventName = "We are lucky";
+const titleCard = "Dăm ba kì thi , easy, so easy ";
+const contentCard =
+  "Chúc chị thi tốt, rất nhiều game kinh dị đang chờ phá đảo. Uy tín!";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "hi.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
